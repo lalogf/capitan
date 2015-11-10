d27770280c06835d9965ec8dc91b9f6748a7fee3
Giancarlo Corzo <gian.corzo@antartec.com>
2015-11-10 21:48:59 +0000

    Cambiando para que salgan las instrucciones

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,7 +22,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       :email, :phone1, :phone2, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:dni, :name, :lastname1, :lastname2, :branch_id, :district, :age, :facebook_username,
+      u.permit(:dni,:code, :name, :lastname1, :lastname2, :branch_id, :district, :age, :facebook_username,
         :email, :phone1, :phone2, :password, :password_confirmation, :current_password)
     end
   end  
