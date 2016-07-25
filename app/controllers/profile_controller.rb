fe98acd09905c29d87d6add9b9b2a6807a646a0d
Iván Medina Díaz <medinadiazivan@gmail.com>
2016-07-25 22:59:47 +0000

    Order by lesson id

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,7 @@ class ProfileController < ApplicationController
     
     def myprofile
         @reviews = Review.where(user_id: current_user.id)
-        @pages = Page.where(page_type: "codereview")
+        @pages = Page.where(page_type: "codereview").order(:lesson_id)
     end
     
 end
