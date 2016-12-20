3aee4d2b6e886f943286ded13336f0d62a3cb953
Giancarlo Corzo <gian.corzo@gmail.com>
2016-12-20 11:04:53 -0500

    latest

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -17,27 +17,27 @@ class Submission < ActiveRecord::Base
   belongs_to :page
 
   def self.avg_classroom_points group_id, sprint_id
-    Submission.joins(:user).
-               joins(:page => :sprints).
-               where("(sprint_pages.points > 0 or pages.points > 0) and pages.page_type not in ('material','score')").
-               where("sprints.id = ?", sprint_id).
-               where("users.disable = 0").
-               where("users.group_id = ?",group_id).
-               group("users.id").
-               pluck("round(sum(submissions.points))").
-               reduce [ 0.0, 0 ] do |(s, c), e| [ s + e, c + 1 ] end.reduce(&:/).round
+    submissions = Submission.joins(:user).
+                  joins(:page => :sprints).
+                  where("(sprint_pages.points > 0 or pages.points > 0) and pages.page_type not in ('material','score')").
+                  where("sprints.id = ?", sprint_id).
+                  where("users.disable = 0").
+                  where("users.group_id = ?",group_id).
+                  group("users.id").
+                  pluck("round(sum(submissions.points))")
+    return if submissions != nil and !submissions.empty? ? (submissions.inject(0.0) { |sum,el| sum + el }.to_f / submissions.size).round : 0
   end
 
   def self.avg_all_classroom_points group_id
-    Submission.joins(:user).
-               joins(:page => {:sprints => :group}).
-               where("(sprint_pages.points > 0 or pages.points > 0) and pages.page_type not in ('material','score')").
-               where("users.disable = 0").
-               where("users.group_id = ?",group_id).
-               where("groups.id = ?",group_id).
-               group("users.id").
-               pluck("round(sum(submissions.points))").
-               reduce [ 0.0, 0 ] do |(s, c), e| [ s + e, c + 1 ] end.reduce(&:/).round
+    submissions = Submission.joins(:user).
+                  joins(:page => {:sprints => :group}).
+                  where("(sprint_pages.points > 0 or pages.points > 0) and pages.page_type not in ('material','score')").
+                  where("users.disable = 0").
+                  where("users.group_id = ?",group_id).
+                  where("groups.id = ?",group_id).
+                  group("users.id").
+                  pluck("round(sum(submissions.points))")
+      return if submissions != nil and !submissions.empty? ? (submissions.inject(0.0) { |sum,el| sum + el }.to_f / submissions.size).round : 0
   end
 
   def self.students_technical_points group_id
