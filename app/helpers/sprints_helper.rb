1d1766bdeb5757e68d929b6fc1ab7486c0df06b2
Giancarlo Corzo <gian.corzo@gmail.com>
2017-01-02 12:23:01 -0500

    Cleaning helpers

--- app/helpers/sprints_helper.rb
+++ app/helpers/sprints_helper.rb
@@ -3,7 +3,6 @@ module SprintsHelper
   def select_sprint_page sprint_pages_ids, page_id
     return "" if sprint_pages_ids == nil
     sp = sprint_pages_ids.select { |sp| sp[0] == page_id }
-    puts sp
     return !sp.empty? ? sp.first[1] : ""
   end
 
