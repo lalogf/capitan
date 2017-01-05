9296151239237312f39601ab38ecdfc6429328f8
Giancarlo Corzo <gian.corzo@gmail.com>
2017-01-05 16:58:32 -0500

    hardcoding track for a 20171 group

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
 
 def after_sign_in_path_for(resource)
-  resource.applicant? ? selection_url : resource.employer? ? employer_dashboard_coders_url : show_track_url(1)
+  resource.applicant? ? selection_url : resource.employer? ? employer_dashboard_coders_url : resource.group_id = 15 ? show_track_url(2) : show_track_url(1)
 end
 
 end
