ded3f9c6be6c28698b008512ae1011346444460a
ivandevp <medinadiazivan@gmail.com>
2017-01-30 08:41:03 -0600

    MX 2017 group validation

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,8 @@
 class Users::SessionsController < Devise::SessionsController
 
 def after_sign_in_path_for(resource)
-  resource.applicant? ? selection_url : resource.employer? ? employer_dashboard_coders_url : resource.group_id = 15 ? show_track_url(2) : show_track_url(1)
+  new_groups = [15, 16]
+  resource.applicant? ? selection_url : resource.employer? ? employer_dashboard_coders_url : new_groups.include?(resource.group_id)  ? show_track_url(2) : show_track_url(1)
 end
 
 end
